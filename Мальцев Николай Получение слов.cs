901d04f1ab0bd4c34af5ec707d812a59aa939afb
NikolayMaltsev14022002 <72619291+NikolayMaltsev14022002@users.noreply.github.com>
2020-10-29 11:31:55 +0500

    Add files via upload

---
+++ Мальцев Николай Получение слов.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SymbolsAndStrings
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            var ship = "теплоход";
+            var thing =  " " + ship[7] + ship[1] + ship.Substring(3, 2);
+            var coldness = ship.Substring(5, 2) + ship.Substring(3, 2) + ship[7];
+            Console.WriteLine($"Из слова теплоход мы получили слова:{coldness},{thing}");
+
+            Console.ReadKey();
+        }
+    }
+}
